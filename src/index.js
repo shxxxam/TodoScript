8dfe284842087c0f6466206a2c1a74215558ca8f
Shubham Thakur <73770319+shxxxam@users.noreply.github.com>
2024-05-27 14:08:13 +0530

    Removed BrowserRouter tags

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
